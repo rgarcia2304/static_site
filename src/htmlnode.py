39f2a65a4b2eb5a253c88fcbe338c5ee65a77a17
rgarcia2304 <rgarcia22141@gmail.com>
2025-10-23 12:03:02 -0400

    added the child node functionallity

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -49,5 +49,25 @@ class LeafNode(HTMLNode):
                 return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
 
 
+class ParentNode(HTMLNode):
 
+    def __init__(self, tag, children,props = None):
+
+        super().__init__(tag=tag, children = children, props=props, value=None)
+
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("No tag")
+            
+        if self.children is None:
+            raise ValueError("No children")
+            
+        result_string = ""
+        for child in self.children:
+            result = child.to_html()
+            result_string += result
+            result_string = f'<{self.tag}>{result_string}</{self.tag}>'
+            
+        return result_string
 
